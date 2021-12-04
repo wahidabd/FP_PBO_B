e3e8aa68eadc4b0dee94711080699db5e011003a
Abd Wahid <wahed.blog99@gmail.com>
2021-12-04 20:50:44 +0700

    add: class welcome screen

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,64 @@
 //Part1 Start
-import java.awt.Color;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class Main {
-	
+public class Main extends JWindow {
+
+	private int duration;
+	private JProgressBar progressBar;
+	private Welcome welcome;
+	private JPanel panel;
+
+	public Main(int d){
+		duration = d;
+		setSize(500, 300);
+		setLocationRelativeTo(null);
+	}
+
+	public void showWelcome(){
+		welcome = new Welcome();
+
+		panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 40));
+		panel.setOpaque(false);
+
+		progressBar = new JProgressBar(0, 100);
+		progressBar.setStringPainted(false);
+		progressBar.setPreferredSize(new Dimension(getWidth() -10, 15));
+		progressBar.setForeground(Color.GREEN);
+		progressBar.setVisible(true);
+		panel.add(progressBar);
+
+		welcome.add(progressBar, "Top");
+		getContentPane().add(welcome, "Center");
+
+		setVisible(true);
+
+		for (int i = 0; i < 100; i++) {
+			try {
+				progressBar.setValue(i);
+				Thread.sleep(duration);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		setVisible(false);
+	}
+
+	public void showAndWelcome(){
+		showWelcome();
+		dispose();
+	}
+
 	public static void main(String[] args) {
+		// Welcome Scree
+		Main welcome = new Main(30);
+		welcome.showAndWelcome();
+
+		// Game Play
 		JFrame obj = new JFrame();
 		GamePlay gameplay = new GamePlay();
-		
+
 		obj.setBounds(10,10,905,700);
 		obj.setBackground(Color.DARK_GRAY);
 		obj.setResizable(false);
---
+++ src/Welcome.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Welcome extends JPanel {
+
+    public Welcome(){
+        //splash
+        JWindow window = new JWindow();
+//        window.getContentPane().add(new JLabel("Welcome Snake Game....", SwingConstants.CENTER));
+//        window.setBounds(10, 10,  905, 700);
+//        window.setVisible(true);
+        window.setBackground(Color.DARK_GRAY);
+
+    }
+
+}
