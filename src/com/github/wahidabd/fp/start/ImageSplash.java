73fde5a10dc45cabe4346fc17ccfb38b5e419562
khalidjuna <khalidjunata@gmail.com>
2021-12-17 22:38:13 +0700

    extract class ImageSplash from class welcome

--- src/com/github/wahidabd/fp/start/ImageSplash.java
+++ src/com/github/wahidabd/fp/start/ImageSplash.java
@@ -1,5 +1,21 @@
 package com.github.wahidabd.fp.start;
 
-public class ImageSplash {
+import javax.swing.*;
+import java.awt.*;
 
+public class ImageSplash extends JPanel {
+
+    private final ImageIcon image;
+
+    public ImageSplash(){
+        image = new ImageIcon("assets/loading_Screen.png");
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        image.paintIcon(this, g, 0, 0);
+
+    }
 }
